fd3bc9f61ba68ec0d60ae7018cd0a654b585712e
Ryan Hastings <rhaasti@gmail.com>
2021-08-19 22:36:06 -0400

    added users to items table and item_cycle_counts table, set up authentication for root page

--- app/models/item_cycle_count.rb
+++ app/models/item_cycle_count.rb
@@ -1,4 +1,5 @@
 class ItemCycleCount < ApplicationRecord
   belongs_to :item
   belongs_to :cycle_count
+  belongs_to :user
 end
---
+++ db/migrate/20210820020644_remove_last_name_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveLastNameFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :last_name, :string
+  end
+end
