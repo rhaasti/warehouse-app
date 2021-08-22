2778c7432b69a840f0aeab5003a203006d331c4a
Ryan Hastings <rhaasti@gmail.com>
2021-08-22 14:19:56 -0400

    added null:false for user_id on items table and item_cycle_counts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_021514) do
+ActiveRecord::Schema.define(version: 2021_08_22_164956) do
 
   create_table "cycle_counts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2021_08_20_021514) do
     t.integer "cycle_count_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["cycle_count_id"], name: "index_item_cycle_counts_on_cycle_count_id"
     t.index ["item_id"], name: "index_item_cycle_counts_on_item_id"
     t.index ["user_id"], name: "index_item_cycle_counts_on_user_id"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_08_20_021514) do
     t.integer "minimum"
     t.integer "vendor_id", null: false
     t.decimal "value"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
     t.index ["vendor_id"], name: "index_items_on_vendor_id"
   end
