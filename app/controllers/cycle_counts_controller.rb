e7bdb4169a160e3fdb3be62f6ccfb38cc8ccfbee
Ryan Hastings <rhaasti@gmail.com>
2021-09-12 16:40:24 -0400

    added 'remaining items' on the cycle count page, so as user selects an item it is removed from a table of cards.

--- app/controllers/cycle_counts_controller.rb
+++ app/controllers/cycle_counts_controller.rb
@@ -5,6 +5,7 @@ class CycleCountsController < ApplicationController
   end
 
   def show
+    @items = Item.all
     @cycle_count = CycleCount.find_by(id:params[:id])
     @all_items = ItemCycleCount.where(cycle_count_id: @cycle_count)
     @item_cycle_count = ItemCycleCount.new
